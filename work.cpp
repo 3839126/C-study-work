302adf2ddf62cac801f07a4f7c99b21be6d951be
3839126 <39831209+3839126@users.noreply.github.com>
2019-11-19 09:07:55 +0800

    Add files via upload

---
+++ work.cpp
@@ -0,0 +1,35 @@
+﻿//输入字符串判断是否为回文-“对称的字符串”
+#include <stdio.h>
+int main() {
+	int i, length, bool1 = 0, max;
+	char a, ch[15];
+	for (i = 0;; i++) {//循环输入
+		scanf_s("%c", &a);
+		if (a != '#') {
+			ch[i] = a;
+			length = i;
+			max = i + 1;
+		}
+		else
+			break;
+	}
+	printf("%d", max);
+	// length--;
+	for (i = 0;; i++, length--) {
+		if (i>=((max/2)-1)||length<=((max/2)-1))
+		{
+			break;
+		}
+		else if (ch[i] != ch[length]) {
+			bool1 = 0;
+			break;
+		}
+		else {
+			bool1 = 1;
+		}
+	}
+	if (bool1 == 1)
+		printf("是");
+	else
+		printf("不是");
+}
